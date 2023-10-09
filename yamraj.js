4f09a6f03d3febf529000c1e5a616780d1ec1434
YamrajShrestha <info.yamrajshrestha@gmail.com>
2023-10-09 16:30:32 +0545

    added rest operator

--- yamraj.js
+++ yamraj.js
@@ -4,3 +4,14 @@ arr.forEach((item) => {
   sum = sum + item;
 });
 console.log(sum);
+
+const user = {
+  name: "ram",
+  address: "kathmandu",
+  age: 22,
+  code: "KY01P",
+  pin: 44600,
+};
+
+const { pin, code, ...rest } = user;
+console.log(rest);
